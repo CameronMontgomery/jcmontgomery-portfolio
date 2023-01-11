5045d404f8e4c38074a50ed60f80b19790a513cf
Cameron Montgomery <jc.montgomery0102@gmail.com>
2023-01-11 12:40:11 -0700

    Create App component with Outlet for pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,17 @@
-import NavBar from "./NavBar";
+import { Outlet } from 'react-router-dom';
+import NavBar from './NavBar';
+import Footer from '../components/Footer';
 
 const App = () => {
-  return <NavBar />;
+  return (
+    <div className='relative min-h-screen'>
+      <NavBar />
+      <div className='pb-11'>
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
