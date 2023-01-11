5d91548da0f7b582a6a8ff43720334aa9d50abe9
Cameron Montgomery <jc.montgomery0102@gmail.com>
2023-01-11 13:10:14 -0700

    Change styling to improve responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className='absolute bottom-0 w-full rounded-t-lg bg-white p-4 shadow dark:bg-gray-800 md:flex md:items-center md:justify-between md:p-6'>
+    <footer className='absolute bottom-0 w-full rounded-t-lg bg-white p-4 text-center shadow dark:bg-gray-800 md:flex md:items-center md:justify-between md:p-6'>
       <span className='text-sm text-gray-500 dark:text-gray-400 sm:text-center'>
         © 2022{' '}
         <Link href='/' className='hover:underline'>
@@ -10,7 +10,7 @@ const Footer = () => {
         </Link>
         - All Rights Reserved.
       </span>
-      <ul className='mt-3 flex flex-wrap items-center text-sm text-gray-500 dark:text-gray-400 sm:mt-0'>
+      <ul className='relative mt-3 block flex-wrap text-sm text-gray-500 dark:text-gray-400 sm:mt-0 md:flex md:items-center'>
         <li>
           <Link to='about/' className='mr-4 hover:underline md:mr-6 '>
             About
@@ -27,7 +27,7 @@ const Footer = () => {
           </Link>
         </li>
         <li>
-          <Link href='/' className='hover:underline'>
+          <Link href='/' className='mr-4 hover:underline'>
             Contact
           </Link>
         </li>
