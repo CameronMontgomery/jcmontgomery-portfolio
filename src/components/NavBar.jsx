7b0408de4512b0079ce239cc4081834597061a9a
Cameron Montgomery <jc.montgomery0102@gmail.com>
2023-01-12 14:55:40 -0700

    Update styling for dark mode

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,30 +5,32 @@ const NavBar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <div className='flex items-center justify-between border-b border-gray-500 py-5'>
-      <h1 className='font-gruppo ml-8 text-2xl sm:text-3xl'>JC Montgomery</h1>
+    <div className='flex items-center justify-between border-b border-gray-500 py-5 dark:bg-gray-800'>
+      <h1 className='font-gruppo ml-8 text-2xl dark:text-white sm:text-3xl'>
+        <Link to='/'>JC Montgomery</Link>
+      </h1>
       <nav className=''>
         <section className='flex lg:hidden'>
           <button
             className={`group z-50 mr-8 space-y-1.5 ${
               isNavOpen
-                ? ` rounded-3xl bg-gray-300 px-1 pb-2 pt-4 drop-shadow-md`
+                ? ` rounded-3xl bg-gray-300 px-1 pb-2 pt-4 drop-shadow-md dark:bg-gray-700`
                 : ''
             }`}
             onClick={() => setIsNavOpen(!isNavOpen)}
           >
             <span
-              className={`block h-1 w-6 origin-center rounded-sm bg-gray-500 duration-500 ${
+              className={`block h-1 w-6 origin-center rounded-sm bg-gray-500 duration-500 dark:bg-white ${
                 isNavOpen ? 'translate-to-x-top' : ''
               }`}
             ></span>
             <span
-              className={`h-1 w-6 rounded-sm bg-gray-500 duration-300 group-hover:translate-x-2 group-hover:bg-red-600 ${
+              className={`h-1 w-6 rounded-sm bg-gray-500 duration-300 group-hover:translate-x-2 group-hover:bg-red-600 dark:bg-white ${
                 isNavOpen ? 'opacity-0' : 'block'
               }`}
             ></span>
             <span
-              className={`block h-1 w-6 origin-center rounded-sm bg-gray-500 duration-500 ${
+              className={`block h-1 w-6 origin-center rounded-sm bg-gray-500 duration-500 dark:bg-white ${
                 isNavOpen ? 'translate-to-x-bottom' : ''
               }`}
             ></span>
@@ -38,11 +40,17 @@ const NavBar = () => {
               <li>
                 <Link to={'about'}>About</Link>
               </li>
-              <li></li>
+              <li>Test</li>
               <li>Test</li>
             </ul>
           </div>
         </section>
+        {/*
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul> */}
       </nav>
     </div>
   );
