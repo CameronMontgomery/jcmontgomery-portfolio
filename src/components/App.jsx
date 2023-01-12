e98944a280bbd6c65358c29a0e56219df9050b6f
Cameron Montgomery <jc.montgomery0102@gmail.com>
2023-01-12 14:54:50 -0700

    Add state for dark mode and conditional className

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from './NavBar';
 import Footer from '../components/Footer';
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
-    <div className='relative min-h-screen'>
+    <div className={`relative min-h-screen ${isDarkMode ? 'dark' : ''}`}>
       <NavBar />
-      <div className='pb-11'>
+      <div className='pb-[41.8em] md:pb-11'>
         <Outlet />
       </div>
       <Footer />
