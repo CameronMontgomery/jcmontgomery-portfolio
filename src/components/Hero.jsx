8b9eb741452be5357d4b919416b5ee438ca4b563
Cameron Montgomery <jc.montgomery0102@gmail.com>
2023-01-15 15:32:13 -0700

    Create Hero and Skills component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,5 @@
+const Hero = () => {
+  return <div className=''></div>;
+};
+
+export default Hero;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,5 @@
+const Skills = () => {
+  return <div className=''></div>;
+};
+
+export default Skills;
